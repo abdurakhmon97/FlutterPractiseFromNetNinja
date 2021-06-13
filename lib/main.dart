45cf7aa3a09a042bdd61725ed621a8b8bfeed031
abdurakhmon97 <abdurakhmon.makh@gmail.com>
2021-06-13 21:20:34 +0500

    Function as parameter, card, custom widget and so on.

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,53 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'quote.dart';
+import 'quote_card.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: NinjaCard(),
-  ));
+  runApp(MaterialApp(home: QuoteList()));
 }
 
-class NinjaCard extends StatefulWidget {
-  const NinjaCard({Key? key}) : super(key: key);
+class QuoteList extends StatefulWidget {
+  const QuoteList({Key? key}) : super(key: key);
 
   @override
-  _NinjaCardState createState() => _NinjaCardState();
+  _QuoteListState createState() => _QuoteListState();
 }
 
-class _NinjaCardState extends State<NinjaCard> {
-  int ninjaLevel = 0;
+class _QuoteListState extends State<QuoteList> {
+  List<Quote> quotes = [
+    Quote(author: "Oscar Wilde", text: 'Be yourself, everyone else is taken'),
+    Quote(
+        author: "Oscar Wilde",
+        text: 'I have nothing to declare except my genius'),
+    Quote(
+        author: "Oscar Wilde",
+        text: 'The truth is rarely pure and never simple'),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey[900],
+      backgroundColor: Colors.grey[200],
       appBar: AppBar(
-        title: Text("Ninja Card"),
-        centerTitle: true,
-        backgroundColor: Colors.grey[850],
-        elevation: 0,
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: (){
-          setState(() {
-            ninjaLevel++;
-          });
-        },
-        child: Icon(
-          Icons.add,
-        ),
-        backgroundColor: Colors.grey[800],
+        title: Text('Awesome Quotes'),
+        backgroundColor: Colors.redAccent,
       ),
       body: Padding(
-        padding: EdgeInsets.fromLTRB(30, 40, 30, 0),
+        padding: EdgeInsets.all(10),
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Center(
-              child: CircleAvatar(
-                backgroundImage: AssetImage('assets/photo.jpg'),
-                radius: 40,
-              ),
-            ),
-            Divider(
-              height: 90,
-              color: Colors.grey[800],
-            ),
-            Text(
-              'NAME',
-              style: TextStyle(
-                color: Colors.grey,
-                letterSpacing: 2,
-              ),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Text(
-              'Jon-Makh',
-              style: TextStyle(
-                color: Colors.amberAccent[200],
-                letterSpacing: 2,
-                fontSize: 28,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            SizedBox(
-              height: 30,
-            ),
-            Text(
-              'CURRENT NINJA LEVEL',
-              style: TextStyle(
-                color: Colors.grey,
-                letterSpacing: 2,
-              ),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Text(
-              '$ninjaLevel',
-              style: TextStyle(
-                color: Colors.amberAccent[200],
-                letterSpacing: 2,
-                fontSize: 28,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            SizedBox(
-              height: 30,
-            ),
-            Row(
-              children: <Widget>[
-                Icon(
-                  Icons.email,
-                  color: Colors.grey[400],
-                ),
-                SizedBox(
-                  width: 10,
-                ),
-                Text(
-                  'john.makh@gmail.com',
-                  style: TextStyle(
-                    color: Colors.grey[400],
-                    fontSize: 18,
-                    letterSpacing: 2,
-                  ),
-                )
-              ],
-            ),
-          ],
+          children: quotes
+              .map((quote) => QuoteCard(
+                  quote: quote,
+                  delete: () {
+                    setState(() {
+                      quotes.remove(quote);
+                    });
+                  }))
+              .toList(),
         ),
       ),
     );
   }
 }
-
-
